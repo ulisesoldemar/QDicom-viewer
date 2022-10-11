99018f157f474c0286238892acecb03b8455bafc
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-10-11 09:33:20 -0500

    Actualizado orden de las vistas

--- ui_form.py
+++ ui_form.py
@@ -30,84 +30,6 @@ class Ui_MainWindow(object):
         self.gridLayout_3.setObjectName(u"gridLayout_3")
         self.gridLayout_2 = QGridLayout()
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.coronalSpinBox = QSpinBox(self.centralwidget)
-        self.coronalSpinBox.setObjectName(u"coronalSpinBox")
-        self.coronalSpinBox.setEnabled(False)
-
-        self.gridLayout_2.addWidget(self.coronalSpinBox, 6, 2, 1, 1)
-
-        self.coronalSlider = QSlider(self.centralwidget)
-        self.coronalSlider.setObjectName(u"coronalSlider")
-        self.coronalSlider.setEnabled(False)
-        self.coronalSlider.setOrientation(Qt.Horizontal)
-
-        self.gridLayout_2.addWidget(self.coronalSlider, 6, 1, 1, 1)
-
-        self.axialSlider = QSlider(self.centralwidget)
-        self.axialSlider.setObjectName(u"axialSlider")
-        self.axialSlider.setEnabled(False)
-        self.axialSlider.setOrientation(Qt.Horizontal)
-
-        self.gridLayout_2.addWidget(self.axialSlider, 2, 1, 1, 1)
-
-        self.sagitalSpinBox = QSpinBox(self.centralwidget)
-        self.sagitalSpinBox.setObjectName(u"sagitalSpinBox")
-        self.sagitalSpinBox.setEnabled(False)
-
-        self.gridLayout_2.addWidget(self.sagitalSpinBox, 2, 5, 1, 1)
-
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-
-        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 3)
-
-        self.coronalView = QGraphicsView(self.centralwidget)
-        self.coronalView.setObjectName(u"coronalView")
-
-        self.gridLayout_2.addWidget(self.coronalView, 5, 0, 1, 3)
-
-        self.label_6 = QLabel(self.centralwidget)
-        self.label_6.setObjectName(u"label_6")
-
-        self.gridLayout_2.addWidget(self.label_6, 2, 0, 1, 1)
-
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.gridLayout_2.addItem(self.horizontalSpacer, 3, 0, 1, 6)
-
-        self.axialView = QGraphicsView(self.centralwidget)
-        self.axialView.setObjectName(u"axialView")
-
-        self.gridLayout_2.addWidget(self.axialView, 1, 0, 1, 3)
-
-        self.axialSpinBox = QSpinBox(self.centralwidget)
-        self.axialSpinBox.setObjectName(u"axialSpinBox")
-        self.axialSpinBox.setEnabled(False)
-
-        self.gridLayout_2.addWidget(self.axialSpinBox, 2, 2, 1, 1)
-
-        self.sagitalView = QGraphicsView(self.centralwidget)
-        self.sagitalView.setObjectName(u"sagitalView")
-
-        self.gridLayout_2.addWidget(self.sagitalView, 1, 3, 1, 3)
-
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
-
-        self.gridLayout_2.addWidget(self.label_2, 0, 3, 1, 3)
-
-        self.sagitalSlider = QSlider(self.centralwidget)
-        self.sagitalSlider.setObjectName(u"sagitalSlider")
-        self.sagitalSlider.setEnabled(False)
-        self.sagitalSlider.setOrientation(Qt.Horizontal)
-
-        self.gridLayout_2.addWidget(self.sagitalSlider, 2, 4, 1, 1)
-
-        self.label_10 = QLabel(self.centralwidget)
-        self.label_10.setObjectName(u"label_10")
-
-        self.gridLayout_2.addWidget(self.label_10, 2, 3, 1, 1)
-
         self.groupBox = QGroupBox(self.centralwidget)
         self.groupBox.setObjectName(u"groupBox")
         self.formLayout = QFormLayout(self.groupBox)
@@ -166,15 +88,93 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.groupBox, 4, 3, 3, 3)
 
+        self.sagitalSpinBox = QSpinBox(self.centralwidget)
+        self.sagitalSpinBox.setObjectName(u"sagitalSpinBox")
+        self.sagitalSpinBox.setEnabled(False)
+
+        self.gridLayout_2.addWidget(self.sagitalSpinBox, 2, 5, 1, 1)
+
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
 
-        self.gridLayout_2.addWidget(self.label_3, 4, 0, 1, 3)
+        self.gridLayout_2.addWidget(self.label_3, 0, 7, 1, 1)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 3)
+
+        self.axialSlider = QSlider(self.centralwidget)
+        self.axialSlider.setObjectName(u"axialSlider")
+        self.axialSlider.setEnabled(False)
+        self.axialSlider.setOrientation(Qt.Horizontal)
+
+        self.gridLayout_2.addWidget(self.axialSlider, 2, 1, 1, 1)
+
+        self.axialSpinBox = QSpinBox(self.centralwidget)
+        self.axialSpinBox.setObjectName(u"axialSpinBox")
+        self.axialSpinBox.setEnabled(False)
+
+        self.gridLayout_2.addWidget(self.axialSpinBox, 2, 2, 1, 1)
+
+        self.label_10 = QLabel(self.centralwidget)
+        self.label_10.setObjectName(u"label_10")
+
+        self.gridLayout_2.addWidget(self.label_10, 2, 3, 1, 1)
+
+        self.sagitalView = QGraphicsView(self.centralwidget)
+        self.sagitalView.setObjectName(u"sagitalView")
+
+        self.gridLayout_2.addWidget(self.sagitalView, 1, 3, 1, 3)
+
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout_2.addWidget(self.label_2, 0, 3, 1, 3)
+
+        self.label_6 = QLabel(self.centralwidget)
+        self.label_6.setObjectName(u"label_6")
+
+        self.gridLayout_2.addWidget(self.label_6, 2, 0, 1, 1)
+
+        self.sagitalSlider = QSlider(self.centralwidget)
+        self.sagitalSlider.setObjectName(u"sagitalSlider")
+        self.sagitalSlider.setEnabled(False)
+        self.sagitalSlider.setOrientation(Qt.Horizontal)
+
+        self.gridLayout_2.addWidget(self.sagitalSlider, 2, 4, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.gridLayout_2.addItem(self.horizontalSpacer, 3, 0, 1, 6)
+
+        self.coronalSpinBox = QSpinBox(self.centralwidget)
+        self.coronalSpinBox.setObjectName(u"coronalSpinBox")
+        self.coronalSpinBox.setEnabled(False)
+
+        self.gridLayout_2.addWidget(self.coronalSpinBox, 2, 8, 1, 1)
+
+        self.axialView = QGraphicsView(self.centralwidget)
+        self.axialView.setObjectName(u"axialView")
+
+        self.gridLayout_2.addWidget(self.axialView, 1, 0, 1, 3)
+
+        self.coronalSlider = QSlider(self.centralwidget)
+        self.coronalSlider.setObjectName(u"coronalSlider")
+        self.coronalSlider.setEnabled(False)
+        self.coronalSlider.setOrientation(Qt.Horizontal)
+
+        self.gridLayout_2.addWidget(self.coronalSlider, 2, 7, 1, 1)
 
         self.label_8 = QLabel(self.centralwidget)
         self.label_8.setObjectName(u"label_8")
 
-        self.gridLayout_2.addWidget(self.label_8, 6, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.label_8, 2, 6, 1, 1)
+
+        self.coronalView = QGraphicsView(self.centralwidget)
+        self.coronalView.setObjectName(u"coronalView")
+
+        self.gridLayout_2.addWidget(self.coronalView, 1, 6, 1, 3)
 
 
         self.gridLayout_3.addLayout(self.gridLayout_2, 0, 0, 1, 1)
@@ -218,10 +218,6 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(shortcut)
         self.actionDICOMDIR.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+Shift+O", None))
 #endif // QT_CONFIG(shortcut)
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Plano axial", None))
-        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Corte", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Plano sagital", None))
-        self.label_10.setText(QCoreApplication.translate("MainWindow", u"Corte", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Controlador espectral", None))
         self.label_7.setText(QCoreApplication.translate("MainWindow", u"Rango espectral:", None))
         self.minLineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"-", None))
@@ -230,6 +226,10 @@ class Ui_MainWindow(object):
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"L\u00edmite inferior:", None))
         self.label_5.setText(QCoreApplication.translate("MainWindow", u"L\u00edmite superior:", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Plano coronal", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Plano axial", None))
+        self.label_10.setText(QCoreApplication.translate("MainWindow", u"Corte", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Plano sagital", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Corte", None))
         self.label_8.setText(QCoreApplication.translate("MainWindow", u"Corte", None))
         self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
         self.menuAbrir.setTitle(QCoreApplication.translate("MainWindow", u"Abrir", None))
